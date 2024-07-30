b4e5eaa882c48d4b55c4260fa188e6f26f6420ff
minjeoong <orange_mj@naver.com>
2024-07-31 01:07:01 +0900

    ✨ Feat: 카카오 간편인증 2차 쿼리 설정

---
+++ src/api/queries/auth/auth-kakao-verify.ts
@@ -0,0 +1,33 @@
+import {
+  UseMutationOptions,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { PATH } from '../../../routes/path';
+import { PATH_API } from '../../path';
+import { axiosInstance } from '../../axios';
+import { QUERY_KEY } from '../../queryKeys';
+import { setSession } from '@/api/api_utils';
+import { useRouter } from 'next/navigation';
+
+// 카카오 간편인증 1차
+export const useAuthKaKao = <T>(
+  options?: Omit<UseMutationOptions<any, any, T>, 'mutationKey'>,
+) => {
+  const queryClient = useQueryClient();
+  const navigate = useRouter();
+
+  return useMutation({
+    mutationKey: [QUERY_KEY.KAKAO_VERIFY],
+    mutationFn: async (payload: T) => {
+      const response = await axiosInstance.post(PATH_API.KAKAO_VERIFY, payload);
+      return response.data;
+    },
+    onSuccess: (data) => {
+      setSession(data);
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.KAKAO_VERIFY] });
+    },
+    onError: (error) => {},
+    ...options,
+  });
+};
