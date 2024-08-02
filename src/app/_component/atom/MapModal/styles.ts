bd267684f2a0038d2dce3cb5bce2ffe21cc38f42
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-08-03 04:17:11 +0900

    🐛 fix :: 병원 조회 모달

--- src/app/_component/atom/MapModal/styles.ts
+++ src/app/_component/atom/MapModal/styles.ts
@@ -22,7 +22,7 @@ export const ModalContainer = styled.div`
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   z-index: 1000;
   overflow: hidden;
-  margin-bottom: -300px;
+  margin-bottom: -280px;
 `;
 
 export const Bar = styled.div`
