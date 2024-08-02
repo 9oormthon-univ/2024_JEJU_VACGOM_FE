f5139124ff5f30b9a6ba7e8ff3041a77d52df5a6
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-08-02 22:37:57 +0900

    🐛 fix :: 맵에서 병원 정보 모달 발생하는 위치 변경

--- src/app/_component/atom/MapModal/styles.ts
+++ src/app/_component/atom/MapModal/styles.ts
@@ -23,7 +23,7 @@ export const ModalContainer = styled.div`
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   z-index: 1000;
   overflow: hidden;
-  margin-bottom: -400px;
+  margin-bottom: -800px;
 `;
 
 export const Bar = styled.div`
