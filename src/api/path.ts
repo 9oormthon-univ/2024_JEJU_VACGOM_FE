43acfc947db7d23c8686470f24d86c30b732fd96
minjeoong <orange_mj@naver.com>
2024-08-02 22:14:08 +0900

    ✨ Feat: 백곰인증서 디테일 member api 연동

--- src/api/path.ts
+++ src/api/path.ts
@@ -1,6 +1,8 @@
 export const PATH_API = {
   API_DOMAIN: 'https://api-dev-v2.vacgom.co.kr',
   // auth
+  // member
+  MEMBER: '/api/v2/member',
   // 아기 생성 및 백신 조회 후 저장
   SIGNUP: '/api/v2/auth/signup',
   // Dummy Member Request (회원 role 별 토큰발급)
@@ -9,6 +11,8 @@ export const PATH_API = {
   KAKAO: '/api/v2/auth/simpleAuth/kakao',
   // 카카오 간편인증 2차 요청
   KAKAO_VERIFY: '/api/v2/auth/simpleAuth/kakao/verify',
+
+  // vaccine
   // 백신 인증서 조회
   INOCULATIONS: '/api/v2/inoculations',
   INOCULATIONS_DETAIL: '/api/v2/inoculation',
