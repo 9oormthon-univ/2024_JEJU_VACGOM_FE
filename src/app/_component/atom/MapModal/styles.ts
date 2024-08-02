5726aa6712031417fc59b118f496fde08f0090bf
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-08-03 04:24:36 +0900

    🐛 fix :: 모달 제발 찐막 수정

--- src/app/_component/atom/MapModal/styles.ts
+++ src/app/_component/atom/MapModal/styles.ts
@@ -22,7 +22,7 @@ export const ModalContainer = styled.div`
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   z-index: 1000;
   overflow: hidden;
-  margin-bottom: -480px;
+  margin-bottom: -100px;
 `;
 
 export const Bar = styled.div`
